36089086b1e65283a416b31a73f5473ea260b9dd
ShaunLWM <shaunlimweimin@gmail.com>
2020-08-09 00:50:38 +0800

    Add logging to CarousellUtils

--- src/utils/CarousellUtils.js
+++ src/utils/CarousellUtils.js
@@ -1,5 +1,6 @@
 module.exports = {
 	autoExpandReadMore: () => {
+		console.log("[CarousellUtils]: autoExpandReadMore");
 		const divs = document.getElementsByTagName("button");
 		for (let i = 0; i < divs.length; i += 1) {
 			if (divs[i].innerHTML.indexOf("read more") > -1) {
@@ -8,6 +9,7 @@ module.exports = {
 		}
 	},
 	removeSpotlightListings: () => {
+		console.log("[CarousellUtils]: removeSpotlightListings");
 		Array.from(document.querySelectorAll("#IconSpotlight")).map((link) =>
 			link.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.remove()
 		);
