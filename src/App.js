100989611d76c44212d2cd716dc7a72f3e018ab5
ShaunLWM <shaunlimweimin@gmail.com>
2020-08-08 23:24:27 +0800

    localStorage & UI - AutoExpandReadMore

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+	const [settings, setSettings] = useState({
+		autoExpandReadMore: false,
+		accounts: [],
+	});
+
+	useEffect(() => {
+		const localSettings = window.localStorage.getItem("settings");
+		try {
+			if (localSettings) setSettings(JSON.parse(localSettings));
+		} catch (error) {}
+	}, []);
+
+	const onCheckedChanged = (key, value) => {
+		setSettings((prev) => {
+			const newValue = { ...prev };
+			newValue[key] = value;
+			window.localStorage.setItem("settings", JSON.stringify(newValue));
+			return newValue;
+		});
+	};
+
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+			<div>
+				<span>Auto click "read more" in Listing</span>
+				<input
+					type="checkbox"
+					checked={settings["autoExpandReadMore"]}
+					onClick={(e) => onCheckedChanged("autoExpandReadMore", e.target.checked)}
+				/>
+			</div>
 		</div>
 	);
 }
