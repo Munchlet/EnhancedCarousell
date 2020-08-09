83e639b87f81623892f7b2944b9cecd79b39de8d
ShaunLWM <shaunlimweimin@gmail.com>
2020-08-09 12:13:54 +0800

    Choose best storage settings

--- src/utils/storageUtil.js
+++ src/utils/storageUtil.js
@@ -1,8 +1,11 @@
 /* global chrome */
+// chrome.storage.sync is not supported in every Chromium-derivate.
+const storageArea = chrome.storage.sync || chrome.storage.local;
+
 module.exports = {
 	getSettings: () => {
 		return new Promise((resolve, reject) => {
-			chrome.storage.sync.get({ settings: {} }, function (result) {
+			storageArea.get({ settings: {} }, function (result) {
 				console.debug(`[stored] ${JSON.stringify(result)}`);
 				if (result) return resolve(result.settings);
 				return reject(new Error("Failed to retrieve settings"));
