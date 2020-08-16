0295193860730c48de0a8d66083b37cd2bd90e11
ShaunLWM <shaunlimweimin@gmail.com>
2020-08-16 16:10:08 +0800

    Add Block & Unblock button to Profile

--- src/features/inject-profile-block-button.js
+++ src/features/inject-profile-block-button.js
@@ -1,11 +1,27 @@
-const commonUtil = require("../utils/commonUtil");
+import * as CommonUtil from "../utils/commonUtil";
+import OptionsSync from "webext-options-sync";
 
-export default function () {
+const optionsStorage = new OptionsSync();
+
+export default async function () {
 	console.log("[CarousellUtils]: injectBlockProfile");
 	const followerDiv = document.querySelector('a[href$="/followers/"]').parentNode;
-	const node = commonUtil.createIconText("iconDiv__icon iconDiv__icon--block", "Block");
+	// pathname = /{username}
+	// const matches = /https:\/\/[\w+]{2,2}\.carousell\.com\/(.*?)$/g.exec(window.location.href)
+	const username = window.location.pathname.substr(1);
+	let node = null;
+	const { profilesBlock = [] } = await optionsStorage.getAll();
+	const blocked = profilesBlock.includes(username);
+	if (blocked) node = CommonUtil.createIconText("iconDiv__icon iconDiv__icon--block", "Unblock");
+	else node = CommonUtil.createIconText("iconDiv__icon iconDiv__icon--block", "Block");
 	followerDiv.parentNode.insertBefore(node, followerDiv);
-	node.addEventListener("click", function (e) {
-		alert("Thanks!");
+	node.addEventListener("click", async (e) => {
+		if (blocked) {
+			const idx = profilesBlock.indexOf(username);
+			if (idx > -1) profilesBlock.splice(idx, 1);
+		} else profilesBlock.push(username);
+		await optionsStorage.set({ profilesBlock });
+		window.location.reload();
+		// TODO: Prevent reloading somehow
 	});
 }
